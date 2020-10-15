905d619ffe777e8ece3e4b971c3ca1210bc69d18
CaiEddie <>
2020-10-15 18:42:51 -0400

    Revert "Test Kotka stream"
    This reverts commit e7b677591d71b59f7e49cb4e21a26c67ed620c34.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -23,8 +23,8 @@ val teamTopic = "movielog$team"
 val kafkaServer = "fall2020-comp598.cs.mcgill.ca:9092"
 
 fun main(args: Array<String>)  {
-    attachToKafkaServerUsingKotkaClient()
-    //attachToKafkaServerUsingDefaultClient()
+//    attachToKafkaServerUsingKotkaClient()
+    attachToKafkaServerUsingDefaultClient()
 
     val port = 8082
     println("Starting server at ${InetAddress.getLocalHost().hostName}:${port}")
@@ -84,7 +84,6 @@ fun attachToKafkaServerUsingKotkaClient() {
 
     kafka.consumer(topic = teamTopic, threads = 2, messageClass = Message::class) { message ->
         // YOUR CODE GOES HERE
-        message.print(Printed.toSysOut())
     }
 }
 
