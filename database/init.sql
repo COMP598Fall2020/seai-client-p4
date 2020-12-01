b44641b14d3805657131827792a31a9e0c61a1b4
Local User <localuser@fall2020-comp598-4.CS.McGill.CA>
2020-11-30 20:24:52 -0500

    updated kafka movie ratings data

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,35 @@
-CREATE TABLE public.persons (
-    id int PRIMARY KEY,
-    firstName varchar(255),
-    lastName varchar(255)
+CREATE TABLE public.users (
+	user_id serial PRIMARY KEY,
+	user_name VARCHAR(20) UNIQUE NOT NULL
+);
+
+CREATE TABLE public.movies (
+    id serial PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    id_long INT UNIQUE NOT NULL,
+    imdb_id VARCHAR(50),
+    title VARCHAR(50)
+);
+
+CREATE TABLE public.ratings (
+    rid serial PRIMARY KEY,
+    uid INT,
+    mid INT,
+    rating FLOAT,
+    rating_time TIMESTAMP,
+    CONSTRAINT fk_movie
+      FOREIGN KEY(mid) 
+	    REFERENCES public.movies(id_long)
+);
+
+CREATE TABLE public.recommendations (
+    id serial PRIMARY KEY,
+    uid INT,
+    mid INT,
+    ranking_score FLOAT,
+    batch_num INT,
+    recommend_time TIMESTAMP,
+    CONSTRAINT fk_movie
+      FOREIGN KEY(mid) 
+	    REFERENCES public.movies(id_long)
 );
