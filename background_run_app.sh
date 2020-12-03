eb05e2fd29e3167a090faf0d57c75c72efc04621
Jazlyn Hellman <41638801+jazlynhellman@users.noreply.github.com>
2020-12-03 16:50:39 -0500

    updated bash and workflow scripts

--- background_run_app.sh
+++ background_run_app.sh
@@ -3,13 +3,13 @@ killall -9 java
 ./gradlew --stop 
 
 # Run gradlew in background
-nohup ./gradlew run > recommendations.out 2> recommendations.err < /dev/null &
+# nohup ./gradlew run > recommendations.out 2> recommendations.err < /dev/null &
 
 # Call gradle task to connect to Kafka Server and appends to the old data for retraining.
 # This grabs the 'next' 5000 data points from server.
 # This data is synthetic, but it represents the next 5000 points in the real-time 
 # continuous data stream as it would appear in real life.
-# ./gradlew integrationTask
+./gradlew integrationTask
 
 # To update the remote repo with the new data in preparation for next build
 git add .
