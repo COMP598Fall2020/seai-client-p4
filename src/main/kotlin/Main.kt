d12bc03aea2754adeb5556a4f4afc63315a01ad9
Lancer <gjtqiyue@gmail.com>
2020-12-04 20:52:10 -0500

    monitoring plot

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -130,54 +130,98 @@ fun main(args: Array<String>)  {
     }
 }
 
-fun plotPage(rmse, pre, recall, f1, time) {
-    return 
-        "<!DOCTYPE HTML>\n" +
-        "<html>\n" +
-        "<head>\n" +
-        "<script>\n" +
-        "window.onload = function() {\n" +
-        "\n" +
-        "var dataPoints = [];\n" +
-        "\n" +
-        "var chart = new CanvasJS.Chart(\"chartContainer\", {\n" +
-        "\tanimationEnabled: true,\n" +
-        "\ttheme: \"light2\",\n" +
-        "\ttitle: {\n" +
-        "\t\ttext: \"Daily Sales Data\"\n" +
-        "\t},\n" +
-        "\taxisY: {\n" +
-        "\t\ttitle: \"Units\",\n" +
-        "\t\ttitleFontSize: 24,\n" +
-        "\t\tincludeZero: true\n" +
-        "\t},\n" +
-        "\tdata: [{\n" +
-        "\t\ttype: \"column\",\n" +
-        "\t\tyValueFormatString: \"#,### Units\",\n" +
-        "\t\tdataPoints: dataPoints\n" +
-        "\t}]\n" +
-        "});\n" +
-        "\n" +
-        "function addData(data) {\n" +
-        "\tfor (var i = 0; i < data.length; i++) {\n" +
-        "\t\tdataPoints.push({\n" +
-        "\t\t\tx: new Date(data[i].date),\n" +
-        "\t\t\ty: data[i].units\n" +
-        "\t\t});\n" +
-        "\t}\n" +
-        "\tchart.render();\n" +
-        "\n" +
-        "}\n" +
-        "\n" +
-        "\$.getJSON(\"https://canvasjs.com/data/gallery/javascript/daily-sales-data.json\", addData);\n" +
-        "\n" +
-        "}\n" +
-        "</script>\n" +
-        "</head>\n" +
-        "<body>\n" +
-        "<div id=\"chartContainer\" style=\"height: 370px; width: 100%;\"></div>\n" +
-        "<script src=\"https://canvasjs.com/assets/script/jquery-1.11.1.min.js\"></script>\n" +
-        "<script src=\"https://canvasjs.com/assets/script/canvasjs.min.js\"></script>\n" +
-        "</body>\n" +
-        "</html>"
+fun plotPage(rmse:List<Float>, pre:List<Float>, recall:List<Float>, f1:List<Float>, time:List<String>) : String {
+    var rmse_data : String = ""
+    var pre_data : String = ""
+    var recall_data : String = ""
+    var f1_data : String = ""
+
+    for (i in 0..time.size) {
+        val list = time[i].substringBefore(" ").split('-')
+        println(list)
+        rmse_data += "\t\t\t{ x: new Date(" + list[0] + "," + list[1] + "," + list[2] + "), y: " + rmse[i] + " },\n"
+        pre_data += "\t\t\t{ x: new Date(" + list[0] + "," + list[1] + "," + list[2] + "), y: " + pre[i] + " },\n"
+        recall_data += "\t\t\t{ x: new Date(" + list[0] + "," + list[1] + "," + list[2] + "), y: " + recall[i] + " },\n"
+        f1_data += "\t\t\t{ x: new Date(" + list[0] + "," + list[1] + "," + list[2] + "), y: " + f1[i] + " },\n"
+    }
+
+    return "<!DOCTYPE HTML>\n" +
+            "<html>\n" +
+            "<head>  \n" +
+            "<script>\n" +
+            "window.onload = function () {\n" +
+            "\n" +
+            "var chart = new CanvasJS.Chart(\"chartContainer\", {\n" +
+            "\tanimationEnabled: true,\n" +
+            "\ttitle:{\n" +
+            "\t\ttext: \"Model performance over time\"\n" +
+            "\t},\n" +
+            "\taxisX: {\n" +
+            "\t\tvalueFormatString: \"DD MMM,YY\"\n" +
+            "\t},\n" +
+            "\taxisY: {\n" +
+            "\t\ttitle: \"Metric score\",\n" +
+            "\t},\n" +
+            "\tlegend:{\n" +
+            "\t\tcursor: \"pointer\",\n" +
+            "\t\tfontSize: 16,\n" +
+            "\t\titemclick: toggleDataSeries\n" +
+            "\t},\n" +
+            "\ttoolTip:{\n" +
+            "\t\tshared: true\n" +
+            "\t},\n" +
+            "\tdata: [{\n" +
+            "\t\tname: \"RMSE\",\n" +
+            "\t\ttype: \"spline\",\n" +
+            "\t\tshowInLegend: true,\n" +
+            "\t\tdataPoints: [\n" +
+            rmse_data +
+            "\t\t]\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\tname: \"Precision\",\n" +
+            "\t\ttype: \"spline\",\n" +
+            "\t\tshowInLegend: true,\n" +
+            "\t\tdataPoints: [\n" +
+            pre_data +
+            "\t\t]\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\tname: \"Recall\",\n" +
+            "\t\ttype: \"spline\",\n" +
+            "\t\tshowInLegend: true,\n" +
+            "\t\tdataPoints: [\n" +
+            recall_data +
+            "\t\t]\n" +
+            "\t},\n" +
+            "\t{\n" +
+            "\t\tname: \"F1\",\n" +
+            "\t\ttype: \"spline\",\n" +
+            "\t\tshowInLegend: true,\n" +
+            "\t\tdataPoints: [\n" +
+            f1_data +
+            "\t\t]\n" +
+            "\t}]\n" +
+            "});\n" +
+            "chart.render();\n" +
+            "\n" +
+            "function toggleDataSeries(e){\n" +
+            "\tif (typeof(e.dataSeries.visible) === \"undefined\" || e.dataSeries.visible) {\n" +
+            "\t\te.dataSeries.visible = false;\n" +
+            "\t}\n" +
+            "\telse{\n" +
+            "\t\te.dataSeries.visible = true;\n" +
+            "\t}\n" +
+            "\tchart.render();\n" +
+            "}\n" +
+            "\n" +
+            "}\n" +
+            "</script>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "<div id=\"chartContainer\" style=\"height: 370px; width: 100%;\"></div>\n" +
+            "<script src=\"https://canvasjs.com/assets/script/canvasjs.min.js\"></script>\n" +
+            "</body>\n" +
+            "</html>"
+
 }
